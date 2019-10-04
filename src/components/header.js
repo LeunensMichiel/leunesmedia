63a725582e2ba4fc7ee5a344f7f4faff940c4bab
Michiel Leunens <michiel.leunens@gmail.com>
2019-10-04 17:44:10 +0200

    Setup

--- src/components/header.js
+++ src/components/header.js
@@ -3,19 +3,7 @@ import PropTypes from "prop-types"
 import React from "react"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
+  <header>
       <h1 style={{ margin: 0 }}>
         <Link
           to="/"
@@ -27,16 +15,11 @@ const Header = ({ siteTitle }) => (
           {siteTitle}
         </Link>
       </h1>
-    </div>
   </header>
 )
 
 Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
+  btnColor: PropTypes.string,
 }
 
 export default Header
